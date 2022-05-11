69415530d68f7760e1f8ea605097a217295a2fb5
Xingyu Zhang <775847012@qq.com>
2022-05-11 14:18:47 +0800

    Add HelloNumbers.java and HelloWorld.java

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int cum=0;
+        for (int i=0; i<10; i++) {
+            cum = cum + i;
+            System.out.print(cum + " ");
+        }
+        System.out.print("\n");
+    }
+}
